448425afec1723a5daab10ca8c18b31123f7537f
Troy Center <troycenter1@gmail.com>
2017-08-01 13:19:16 -0700

    created project and updated readme.md

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace basic13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        oneto255();
+
+        }
+        static void oneto255(){
+            Console.WriteLine("testing")
+        };
+    }
+}
